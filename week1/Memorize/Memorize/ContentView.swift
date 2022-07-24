290afa2c6a749f17b764ab6e6f8209559097033b
Koo <31850425+gen-com@users.noreply.github.com>
2022-07-25 03:12:41 +0900

    Add Week 1

---
+++ week1/Memorize/Memorize/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Byeongjo Koo on 2022/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let emojis = ["🥺", "🥸", "🤩", "😜", "🧐", "😤", "😛", "🥳", "🥶", "😓", "🫡", "🫥"]
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis, id: \.self, content: { emoji in
+                        CardView(content: emoji).aspectRatio(2 / 3, contentMode: .fit)
+                    })
+                }
+            }
+            Spacer()
+            addCard
+        }
+        .foregroundColor(.red)
+        .padding()
+    }
+    
+    var addCard: some View {
+        Button {
+            // TODO: Add Card
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp = true
+    let content: String
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 5)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ week1/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Byeongjo Koo on 2022/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
