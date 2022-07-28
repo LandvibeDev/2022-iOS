df812b2d4ca706a3c2bc22b4b6de898680d508fa
임주민 <limj1213@gmail.com>
2022-07-28 20:26:42 +0900

    Add title 'Memorize!'

---
+++ week1/Memorize/Memorize/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by 임주민 on 2022/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      VStack{
+        Text("Memorize!")
+          .font(.largeTitle)
+          .padding()
+        Spacer()
+      }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
