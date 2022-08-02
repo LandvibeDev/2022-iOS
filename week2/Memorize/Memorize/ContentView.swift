10ffe0b587a2b315022e223551718197550a2a48
KyungsooLee <jmtkd9196@gmail.com>
2022-08-03 00:18:08 +0900

    Update Week2 Readme.md

---
+++ week2/Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Kyungsoo Lee on 2022/08/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ week2/Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Kyungsoo Lee on 2022/08/02.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservedObject {
+    static let emojis = ["🥺", "🥸", "🤩", "😜", "🧐", "😤", "😛", "🥳", "🥶", "😓", "🫡", "🫥"]
+    
+    }
+    
+    
+}
+
