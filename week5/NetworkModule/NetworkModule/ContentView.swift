d7408edda361d6b8288260f048f77b9c5c7cd701
KyungsooLee <jmtkd9196@gmail.com>
2022-09-06 22:24:58 +0900

    Create NetworkModule project

---
+++ week5/NetworkModule/NetworkModule/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkModule
+//
+//  Created by Kyungsoo Lee on 2022/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ week5/NetworkModule/NetworkModule/NetworkModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkModuleApp.swift
+//  NetworkModule
+//
+//  Created by Kyungsoo Lee on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct NetworkModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
